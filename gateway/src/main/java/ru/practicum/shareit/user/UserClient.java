a9fcac7f5cdfc46321725e7ce902c44f065138df
basarus51 <basarus51@gmail.com>
2025-12-07 18:23:34 +0300

    sprint 16

--- gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -1,18 +1,25 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import ru.practicum.shareit.client.BaseClient;
 import ru.practicum.shareit.user.dto.UserDto;
 
 @Component
 public class UserClient extends BaseClient {
 
-    private static final String API_PREFIX = "/users";
-
-    public UserClient(RestTemplateBuilder builder) {
-        super(builder.rootUri("http://shareit-server:9090" + API_PREFIX).build());
+    public UserClient(@Value("${shareit-server.url}") String serverUrl,
+                      RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + "/users"))
+                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                        .build()
+        );
     }
 
     public ResponseEntity<Object> create(UserDto dto) {
@@ -24,14 +31,14 @@ public class UserClient extends BaseClient {
     }
 
     public ResponseEntity<Object> get(Long userId) {
-        return get("/" + userId);
+        return get(Long.valueOf("/" + userId));
     }
 
     public ResponseEntity<Object> getAll() {
-        return get("");
+        return get(Long.valueOf(""));
     }
 
     public ResponseEntity<Object> delete(Long userId) {
-        return delete("/" + userId);
+        return delete(Long.valueOf("/" + userId));
     }
 }
