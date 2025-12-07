7f05cb60831b45b37c7c00627bf21e8725be8ef3
basarus51 <basarus51@gmail.com>
2025-12-07 15:59:44 +0300

    sprint 16

---
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -0,0 +1,51 @@
+package ru.practicum.shareit.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Slf4j
+public class BaseClient {
+
+    protected final RestTemplate rest;
+
+    protected BaseClient(RestTemplate rest) {
+        this.rest = rest;
+    }
+
+    protected <T> ResponseEntity<Object> post(String path, Long userId, T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
+        return rest.exchange(path, HttpMethod.POST, requestEntity, Object.class);
+    }
+
+    protected <T> ResponseEntity<Object> patch(String path, Long userId, T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
+        return rest.exchange(path, HttpMethod.PATCH, requestEntity, Object.class);
+    }
+
+    protected ResponseEntity<Object> get(String path, Long userId) {
+        HttpEntity<Void> requestEntity = new HttpEntity<>(defaultHeaders(userId));
+        return rest.exchange(path, HttpMethod.GET, requestEntity, Object.class);
+    }
+
+    protected ResponseEntity<Object> get(String path, Long userId, Map<String, Object> params) {
+        HttpEntity<Void> requestEntity = new HttpEntity<>(defaultHeaders(userId));
+        return rest.exchange(path, HttpMethod.GET, requestEntity, Object.class, params);
+    }
+
+    protected ResponseEntity<Object> delete(String path, Long userId) {
+        HttpEntity<Void> requestEntity = new HttpEntity<>(defaultHeaders(userId));
+        return rest.exchange(path, HttpMethod.DELETE, requestEntity, Object.class);
+    }
+
+    private HttpHeaders defaultHeaders(Long userId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        if (userId != null) {
+            headers.add("X-Sharer-User-Id", String.valueOf(userId));
+        }
+        return headers;
+    }
+}
